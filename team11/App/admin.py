726bce2c0a775a044111c67dcb5897f0ac437e56
Hamzashuldum2003 <162129569+Hamzashuldum2003@users.noreply.github.com>
2024-03-08 13:17:07 +0200

    second commit by hamza

---
+++ team11/App/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+'''admin.site.register(Teacher)'''
+
+admin.site.register(Admin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
