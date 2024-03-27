75ed5cb416a8e234bb2f0a02349f488000fb39d7
hassanabuaj <161459030+hassanabuaj@users.noreply.github.com>
2024-03-27 15:46:56 +0200

    Update admin.py

--- team11/App/admin.py
+++ team11/App/admin.py
@@ -4,8 +4,16 @@ from .models import *
 # Register your models here.
 admin.site.register(Teacher)
 admin.site.register(Student)
+admin.site.register(HomeWork)
 admin.site.register(Admin)
+admin.site.register(StudentSolution)
 admin.site.register(AdminMessage)
+admin.site.register(TeacherMessage)
+admin.site.register(Grade)
+admin.site.register(Studies)
+admin.site.register(StudiesStudent)
+admin.site.register(Bugreport)
+admin.site.register(TeacherId)
 
 
 
