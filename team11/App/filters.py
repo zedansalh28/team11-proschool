8d749b016ee4058c23dce2695e417a52702e5256
hassanabuaj <161459030+hassanabuaj@users.noreply.github.com>
2024-03-27 15:40:54 +0200

    Update filters.py

--- team11/App/filters.py
+++ team11/App/filters.py
@@ -3,7 +3,12 @@ from App.models import *
 
 
 
+class StudentSolutionsFilter(django_filters.FilterSet):
+    class Meta:
+        model = StudentSolution
+        fields = ['student','homeWork']
 
+ 
 class userFilter(django_filters.FilterSet):
     class Meta:
         model =User
